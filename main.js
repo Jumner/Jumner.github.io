1519cac7dded5ca9a895549344d35513ebf2b181
Jumner <Jumner@debian>
2021-08-29 17:42:27 -0400

    basic layout done

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert('Js is alive!');
+// My Javascript!
+window.onload = () => {
+	// Wait for page to load
+	let pages = document.getElementsByClassName('page'); // Grab all the pages
+	for(let i = 0; i < pages.length; i ++) {
+		pages[i].style.height = window.innerHeight + 'px'; // Size all the of them
+		pages[i].style.top = window.innerHeight * i + 'px';
+		pages[i].style.backgroundColor = 'rgb(255,' + Math.floor(i*255/(pages.length-1)) + ',0)';
+	}
+}
