3e383909c114f58fc01ea154436b801b4c2a5172
Jumner <Jumner@debian>
2021-08-29 20:54:04 -0400

    added some colours and started page 1

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // My Javascript!
+let pageColours = ['linear-gradient(-45deg, #1f261f, #95af95)'];
+
 window.onload = () => {
 	// Wait for page to load
 	let pages = document.getElementsByClassName('page'); // Grab all the pages
 	for (let i = 0; i < pages.length; i++) {
 		pages[i].style.height = window.innerHeight + 'px'; // Size all the of them
-		pages[i].style.backgroundColor =
-			'rgb(255,' + Math.floor((i * 255) / (pages.length - 1)) + ',0)';
+		if (pageColours[i]) {
+			pages[i].style.background = pageColours[i];
+		} else {
+			pages[i].style.backgroundColor =
+				'rgb(255,' + Math.floor((i * 255) / (pages.length - 1)) + ',0)';
+		}
 	}
+
+	// Position home text box
+	let box = document.getElementById('hometext');
+	let gap = box.style.marginLeft;
+	box.style.marginTop = gap;
 };
