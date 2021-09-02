f11cb1e23b18a85a075a87b7a3671708c6128c6c
Jumner <Jumner@debian>
2021-09-02 16:42:54 -0400

    testing project layout

--- work.js
+++ work.js
@@ -1,5 +1,7 @@
 class project {
-	constructor() {}
+	constructor(name) {
+		this.name = name;
+	}
 
 	get element() {
 		let project = document.createElement('div');
@@ -8,11 +10,31 @@ class project {
 
 		let wrapper = document.createElement('div');
 		wrapper.className = 'wrapper';
-		wrapper.innerHTML = 'lol';
+		// wrapper.innerHTML = this.name;
 		project.appendChild(wrapper);
+
+		let content = document.createElement('div');
+		content.className = 'content';
+		content.innerHTML = 'content: ' + this.name;
+
+		let space = document.createElement('div');
+		space.className = 'space';
+		space.innerHTML = 'space: ' + this.name;
+
+		let header = document.createElement('div');
+		header.className = 'header';
+		header.innerHTML = 'header: ' + this.name;
+
+		wrapper.appendChild(header);
+		wrapper.appendChild(content);
+		wrapper.appendChild(space);
 
 		return project;
 	}
 }
 
-let projects = [new project(), new project()];
+let projects = [
+	new project('My portfolio website, This!'),
+	new project('Turing Vscode'),
+	new project('Do Work'),
+];
