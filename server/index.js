fea049328cbf29767c3dccb337686714451b0f47
Jumner <Jumner@debian>
2021-09-16 19:43:26 -0400

    discord bot works

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
 const http = require('http');
+
+const discord = require('discord.js');
+const { token } = require('./secret.json');
+const client = new discord.Client({
+	intents: ['GUILDS', 'GUILD_MESSAGES'],
+});
+client.on('ready', () => {
+	console.log('Discord bot running');
+});
+client.login(token);
+
 const server = http.createServer((req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	// res.writeHead(420); // For reporting issues
@@ -7,6 +18,13 @@ const server = http.createServer((req, res) => {
 		// Time to handle the data
 		data = JSON.parse(data.toString());
 		console.log(data);
+		// Send a message
+		client.channels.cache.get('888195849119727707').send(
+			`Message from form\n
+				Name:               ${data.Name}
+				Contact info:    ${data.Info}
+				Message:          ${data.Message}`
+		);
 	});
 });
 const port = 8080;
