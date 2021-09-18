1efce46522ba51eb8b027f5fe94187ed961fe50c
Jumner <Jumner@debian>
2021-09-17 22:22:29 -0400

    maybe this will bring localhost and git together

--- main.js
+++ main.js
@@ -285,6 +285,7 @@ function contactSubmit() {
 		// fetch('http://localhost:8080/', {
 		fetch('http://24.150.208.139:8080/', {
 			method: 'POST',
+			referrerPolicy: 'no-referrer',
 			// mode: 'cors',
 			// headers: {
 			// 	'Referer': 'https://jumner.github.io/',
